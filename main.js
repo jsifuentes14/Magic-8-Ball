2be460d7224e6da006106a91631afeb94942ba41
jsifuentes14 <sifuentes_jacob@yahoo.com>
2024-06-25 14:47:25 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+// Magic 8-ball project
+var userName = 'Jacob';
+userName ? console.log(`Hello ${userName}!`) : console.log('Hello!');
+
+var userQuestion = 'Will I have a better car soon?';
+console.log(`${userName} wants to know - ${userQuestion}`);
+
+var randomNumber = Math.floor(Math.random() * 8);
+console.log (randomNumber);
+
+var eightBall = '';
+if (randomNumber = 0); {
+    eightBall = 'It is certain';
+} if (randomNumber = 1); {
+    eightBall = 'It is decidedly so';
+} if (randomNumber = 2); {
+    eightBall = 'Reply hazy try again';
+} if (randomNumber = 3); {
+    eightBall = 'Cannot predict now';
+} if (randomNumber = 4); {
+    eightBall = 'Do not count on it';
+} if (randomNumber = 5); {
+    eightBall = 'My sources say no';
+} if (randomNumber = 6); {
+    eightBall = 'Outlook not so good';
+} if (randomNumber = 7); {
+    eightBall = 'Signs point to yes';
+}
+console.log(eightBall);
+
+
+
+
+
+
+
+
+
